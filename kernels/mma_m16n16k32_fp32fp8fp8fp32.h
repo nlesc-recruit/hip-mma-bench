10d555e96ee52c02c6ff74a60b50f97a1cd066e5
Leon Oostrum <l.oostrum@esciencecenter.nl>
2024-06-25 14:34:49 +0200

    Add initial fp8 implementation
    ToDo:
    - determine correct block sizes to launch with
    - determine correct way to input C matrix

---
+++ kernels/mma_m16n16k32_fp32fp8fp8fp32.h
@@ -0,0 +1,56 @@
+#include <rocwmma/rocwmma.hpp>
+
+#include "precision.h"
+
+using namespace rocwmma;
+
+
+template<>
+class __align__(4) fragment<matrix_a, 16, 16, 32, precision::fp8, row_major> {
+  public:
+    const static size_t num_elements = 2;
+    VRegF32x2 regs;
+};
+
+template<>
+class __align__(4) fragment<matrix_b, 16, 16, 32, precision::fp8, col_major> {
+  public:
+    const static size_t num_elements = 2;
+    VRegF32x2 regs;
+};
+
+template<typename MatrixT, typename DataLayout>
+__device__ void fill_fragment(fragment<MatrixT, 16, 16, 32, precision::fp8, DataLayout>& frag, float value) {
+  for (size_t i = 0; i < frag.num_elements; i++) {
+    frag.regs.data[i] = value;
+  }
+}
+
+// https://gpuopen.com/learn/amd-lab-notes/amd-lab-notes-matrix-cores-readme/#mfma-compiler-intrinsic-syntax
+inline __device__ void mma_sync_llvm(
+  fragment<accumulator, 16, 16, 32, float>& d,
+  const fragment<matrix_a, 16, 16, 32, precision::fp8, row_major>& a,
+  const fragment<matrix_b, 16, 16, 32, precision::fp8, col_major>& b,
+  const fragment<accumulator, 16, 16, 32, float>& c) {
+
+  const size_t M = 16;
+  const size_t N = 16;
+  const size_t K = 32;
+  const size_t output_elements_per_thread = M * N / 64; // wave size 64 assumed
+
+  size_t mk = threadIdx.y + K * threadIdx.x;
+  size_t kn = threadIdx.x + N * threadIdx.y;
+
+  long amk = a.regs.data[mk];
+  long bkn = b.regs.data[kn];
+  VecT<float, 4> dmn;
+  VecT<float, 4> cmn; // todo: what to put for C here?
+#if __gfx940__ || __gfx941__ || __gfx942__
+  dmn.data = __builtin_amdgcn_mfma_f32_16x16x32_fp8_fp8(amk, bkn, cmn.data, 0, 0, 0);
+#endif
+
+  for (size_t i = 0; i < output_elements_per_thread; i++) {
+    const int idx = threadIdx.x + i * N + threadIdx.y * 4 * N;
+    (*d).data[idx] = dmn.data[i];
+  }
+}
---
+++ kernels/precision.h
@@ -0,0 +1,9 @@
+#ifndef PRECISION_H
+#define PRECISION_H
+
+namespace precision {
+  struct fp8; // e4m3
+  struct bf8; // e5m2
+}
+
+#endif // PRECISION_H
