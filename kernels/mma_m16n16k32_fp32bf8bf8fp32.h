1330f2ee7ce4855cb0b888c8ac32f68544e914ea
Leon Oostrum <l.oostrum@esciencecenter.nl>
2024-06-30 00:00:11 +0200

    Add bf8 kernel header

---
+++ kernels/mma_m16n16k32_fp32bf8bf8fp32.h
@@ -0,0 +1,40 @@
+#include <rocwmma/rocwmma.hpp>
+
+#include "precision.h"
+
+using namespace rocwmma;
+
+
+template<>
+class __align__(4) fragment<matrix_a, 16, 16, 32, precision::bf8, row_major> {
+  public:
+    const static size_t num_elements = 2;
+    VRegF32x2 regs;
+};
+
+template<>
+class __align__(4) fragment<matrix_b, 16, 16, 32, precision::bf8, col_major> {
+  public:
+    const static size_t num_elements = 2;
+    VRegF32x2 regs;
+};
+
+template<typename MatrixT, typename DataLayout>
+__device__ void fill_fragment(fragment<MatrixT, 16, 16, 32, precision::bf8, DataLayout>& frag, const char value) {
+    for (size_t i = 0; i < frag.num_elements; i++) {
+        frag.regs.data[i] = value | (value << 8) | (value << 16) | (value << 24);
+    }
+}
+
+// MFMA compiler intrinsic syntax:
+// https://gpuopen.com/learn/amd-lab-notes/amd-lab-notes-matrix-cores-readme/#mfma-compiler-intrinsic-syntax
+inline __device__ void mma_sync_llvm(
+  fragment<accumulator, 16, 16, 32, float>& d,
+  const fragment<matrix_a, 16, 16, 32, precision::bf8, row_major>& a,
+  const fragment<matrix_b, 16, 16, 32, precision::bf8, col_major>& b,
+  const fragment<accumulator, 16, 16, 32, float>& c) {
+
+#if __gfx940__ || __gfx941__ || __gfx942__
+  (*d).data = __builtin_amdgcn_mfma_f32_16x16x32_bf8_bf8(((VRegI64x1 const&)(a.regs)).data[0], ((VRegI64x1 const&)(b.regs)).data[0], (*c).data, 0, 0, 0);
+#endif
+}
