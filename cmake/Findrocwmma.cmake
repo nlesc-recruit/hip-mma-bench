b3fe078260b0fe213834635fb49c6c8c50fa1adc
Bram Veenboer <bram.veenboer@gmail.com>
2024-07-02 10:24:23 +0200

    Add Findrocwmma.cmake

---
+++ cmake/Findrocwmma.cmake
@@ -0,0 +1,16 @@
+include(FindPackageHandleStandardArgs)
+
+find_path(
+  ROCWMMA_INCLUDE_DIR
+  NAMES rocwmma/rocwmma.hpp
+  PATHS /usr/local/include /usr/include)
+
+find_package_handle_standard_args(
+  rocwmma
+  REQUIRED_VARS ROCWMMA_INCLUDE_DIR
+  VERSION_VAR ROCWMMA_VERSION)
+
+if(NOT TARGET rocwmma)
+  add_library(rocwmma INTERFACE)
+  target_include_directories(rocwmma INTERFACE ${ROCWMMA_INCLUDE_DIR})
+endif()
